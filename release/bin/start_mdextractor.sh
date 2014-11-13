139cc3cf9ee1ddf8d8a9eab1fa3e68ec26162707
Jeroen Ticheler <Jeroen.Ticheler@GeoCat.net>
2014-11-13 02:45:50 +0100

    Make app exec for Windows and more

--- release/bin/start_mdextractor.sh
+++ release/bin/start_mdextractor.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-cd `dirname $0`
- ROOT_PATH=`pwd`
- echo $ROOT_PATH
- java -Xms256M -Xmx1024M -cp ../lib/*:../tufts_metadata_generator.start_0_1.Start --context=Default "$@" 
+java -Xms256M -Xmx1024M -cp ../lib/*:../tufts_metadata_generator.start_0_1.Start -jar ../metadata-extractor.jar
